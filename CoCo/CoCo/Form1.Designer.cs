59fb9346bae2d989ccf47751e584e150d8e4ac91
CoderEasonLin <lin07646@gmail.com>
2015-01-16 11:41:03 +0800

    Add CoCo.

---
+++ CoCo/CoCo/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace CoCo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lBox = new System.Windows.Forms.ListBox();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.cbStayOnTop = new System.Windows.Forms.CheckBox();
+            this.gbSetting = new System.Windows.Forms.GroupBox();
+            this.tbItemCount = new System.Windows.Forms.TextBox();
+            this.tbVisibility = new System.Windows.Forms.TrackBar();
+            this.gbSetting.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVisibility)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lBox
+            // 
+            this.lBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.lBox.FormattingEnabled = true;
+            this.lBox.Location = new System.Drawing.Point(4, 2);
+            this.lBox.Name = "lBox";
+            this.lBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lBox.Size = new System.Drawing.Size(109, 264);
+            this.lBox.TabIndex = 0;
+            this.lBox.SelectedIndexChanged += new System.EventHandler(this.lBox_SelectedIndexChanged);
+            this.lBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lBox_KeyDown);
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClearAll.Location = new System.Drawing.Point(60, 283);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(53, 26);
+            this.btnClearAll.TabIndex = 1;
+            this.btnClearAll.Text = "Clear All";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClear.Location = new System.Drawing.Point(4, 283);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(53, 26);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // cbStayOnTop
+            // 
+            this.cbStayOnTop.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbStayOnTop.AutoSize = true;
+            this.cbStayOnTop.Location = new System.Drawing.Point(6, 19);
+            this.cbStayOnTop.Name = "cbStayOnTop";
+            this.cbStayOnTop.Size = new System.Drawing.Size(86, 17);
+            this.cbStayOnTop.TabIndex = 3;
+            this.cbStayOnTop.Text = "Stay On Top";
+            this.cbStayOnTop.UseVisualStyleBackColor = true;
+            this.cbStayOnTop.CheckedChanged += new System.EventHandler(this.cbStayOnTop_CheckedChanged);
+            // 
+            // gbSetting
+            // 
+            this.gbSetting.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.gbSetting.Controls.Add(this.tbItemCount);
+            this.gbSetting.Controls.Add(this.tbVisibility);
+            this.gbSetting.Controls.Add(this.cbStayOnTop);
+            this.gbSetting.Location = new System.Drawing.Point(4, 315);
+            this.gbSetting.Name = "gbSetting";
+            this.gbSetting.Size = new System.Drawing.Size(109, 110);
+            this.gbSetting.TabIndex = 4;
+            this.gbSetting.TabStop = false;
+            this.gbSetting.Text = "Setting";
+            // 
+            // tbItemCount
+            // 
+            this.tbItemCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbItemCount.Location = new System.Drawing.Point(8, 40);
+            this.tbItemCount.Name = "tbItemCount";
+            this.tbItemCount.Size = new System.Drawing.Size(45, 20);
+            this.tbItemCount.TabIndex = 5;
+            this.tbItemCount.Text = "1";
+            this.tbItemCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tbItemCount.TextChanged += new System.EventHandler(this.tbItemCount_TextChanged);
+            // 
+            // tbVisibility
+            // 
+            this.tbVisibility.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbVisibility.Location = new System.Drawing.Point(6, 64);
+            this.tbVisibility.Maximum = 9;
+            this.tbVisibility.Name = "tbVisibility";
+            this.tbVisibility.Size = new System.Drawing.Size(97, 42);
+            this.tbVisibility.TabIndex = 4;
+            this.tbVisibility.Scroll += new System.EventHandler(this.tbVisibility_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(114, 427);
+            this.Controls.Add(this.gbSetting);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.lBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Copy Utility";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.gbSetting.ResumeLayout(false);
+            this.gbSetting.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVisibility)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lBox;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.CheckBox cbStayOnTop;
+        private System.Windows.Forms.GroupBox gbSetting;
+        private System.Windows.Forms.TrackBar tbVisibility;
+        private System.Windows.Forms.TextBox tbItemCount;
+    }
+}
+
---
+++ CoCo/CoCo/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CoCo
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool RegisterHotKey( IntPtr hWnd, int id, uint fsModifiers, Keys vk );
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey( IntPtr hWnd, int id );
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            lBox.Items.Clear();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            while (lBox.SelectedItems.Count > 0)
+                lBox.Items.Remove(lBox.SelectedItems[0]);
+        }
+
+        private void cbStayOnTop_CheckedChanged(object sender, EventArgs e)
+        {
+            TopMost = cbStayOnTop.Checked;
+        }
+
+        private void tbVisibility_Scroll(object sender, EventArgs e)
+        {
+            Opacity = 1 - ((double)tbVisibility.Value * 0.1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegisterHotKey(Handle, 100, 2, Keys.Oemtilde);
+            RegisterHotKey(Handle, 300, 2, Keys.Decimal);
+
+            RegisterHotKey(Handle, 101, 2, Keys.NumPad1);
+            RegisterHotKey(Handle, 102, 2, Keys.NumPad2);
+            RegisterHotKey(Handle, 103, 2, Keys.NumPad3);
+            RegisterHotKey(Handle, 104, 2, Keys.NumPad4);
+            RegisterHotKey(Handle, 105, 2, Keys.NumPad5);
+            RegisterHotKey(Handle, 106, 2, Keys.NumPad6);
+            RegisterHotKey(Handle, 107, 2, Keys.NumPad7);
+            RegisterHotKey(Handle, 108, 2, Keys.NumPad8);
+            RegisterHotKey(Handle, 109, 2, Keys.NumPad9);
+
+            RegisterHotKey(Handle, 201, 2, Keys.D1);
+            RegisterHotKey(Handle, 202, 2, Keys.D2);
+            RegisterHotKey(Handle, 203, 2, Keys.D3);
+            RegisterHotKey(Handle, 204, 2, Keys.D4);
+            RegisterHotKey(Handle, 205, 2, Keys.D5);
+            RegisterHotKey(Handle, 206, 2, Keys.D6);
+            RegisterHotKey(Handle, 207, 2, Keys.D7);
+            RegisterHotKey(Handle, 208, 2, Keys.D8);
+            RegisterHotKey(Handle, 209, 2, Keys.D9);
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            UnregisterHotKey(Handle, 100);
+            UnregisterHotKey(Handle, 300);
+
+            UnregisterHotKey(Handle, 101);
+            UnregisterHotKey(Handle, 102);
+            UnregisterHotKey(Handle, 103);
+            UnregisterHotKey(Handle, 104);
+            UnregisterHotKey(Handle, 105);
+            UnregisterHotKey(Handle, 106);
+            UnregisterHotKey(Handle, 107);
+            UnregisterHotKey(Handle, 108);
+            UnregisterHotKey(Handle, 109);
+
+            UnregisterHotKey(Handle, 201);
+            UnregisterHotKey(Handle, 202);
+            UnregisterHotKey(Handle, 203);
+            UnregisterHotKey(Handle, 204);
+            UnregisterHotKey(Handle, 205);
+            UnregisterHotKey(Handle, 206);
+            UnregisterHotKey(Handle, 207);
+            UnregisterHotKey(Handle, 208);
+            UnregisterHotKey(Handle, 209);
+        }
+
+        private void HK_CopyText()
+        {
+            KeyboardUtil.Event.Send(KeyboardUtil.VKey.VK_CONTROL, 0);
+            KeyboardUtil.Event.Send(KeyboardUtil.VKey.VK_C, 0);
+            KeyboardUtil.Event.Send(KeyboardUtil.VKey.VK_C, 2);
+            KeyboardUtil.Event.Send(KeyboardUtil.VKey.VK_CONTROL, 2);
+
+            // for clipboard data processing
+            System.Threading.Thread.Sleep(100);
+
+            string copyData = "";
+            IDataObject iData = null;
+            try
+            {
+                iData = Clipboard.GetDataObject();
+            }
+            catch (Exception ex)
+            { }
+
+            if (iData != null && iData.GetDataPresent(DataFormats.StringFormat))
+            {
+                copyData = (String)iData.GetData(DataFormats.StringFormat);
+                lBox.Items.Add(copyData);
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            if (m.Msg == WM_HOTKEY)
+            {
+                switch (m.WParam.ToInt32())
+                {
+                    case 100:
+                        HK_CopyText();
+                        break;
+
+                    case 101:
+                    case 102:
+                    case 103:
+                    case 104:
+                    case 105:
+                    case 106:
+                    case 107:
+                    case 108:
+                    case 109:
+                        tbItemCount.Text = (m.WParam.ToInt32() - 100).ToString();
+                        break;
+
+                    case 201:
+                    case 202:
+                    case 203:
+                    case 204:
+                    case 205:
+                    case 206:
+                    case 207:
+                    case 208:
+                    case 209:
+                        tbItemCount.Text = (m.WParam.ToInt32() - 200).ToString();
+                        break;
+
+                    case 300:
+                        lBox.Items.Clear();
+                        break;
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void lBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+                lBox.Items.RemoveAt(lBox.SelectedIndex);
+        }
+
+        private void lBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lBox.SelectedIndex < 0)
+                return;
+
+            int count = Convert.ToInt32(tbItemCount.Text);
+            if (count < 0 || count > 10)
+                count = 1;
+
+            String text = "";
+
+            for (int i = lBox.SelectedIndex; i < lBox.Items.Count; i++)
+            {
+                String item = (String)lBox.Items[i];
+
+                if (item != null)
+                    text += item;
+
+                if (i < lBox.Items.Count - 1 && text.Length > 0)
+                {
+                    if ((i - lBox.SelectedIndex + 1) % count == 0)
+                        text += "\n";
+                    else
+                        text += "\t";
+                }
+            }
+
+            if (text != null)
+                Clipboard.SetText(text, TextDataFormat.Text);
+        }
+
+        private void tbItemCount_TextChanged(object sender, EventArgs e)
+        {
+            if (lBox.Items.Count > 0)
+            {
+                if (lBox.SelectedIndex >= 0)
+                    lBox_SelectedIndexChanged(this.lBox, EventArgs.Empty);
+                else
+                    lBox.SelectedIndex = 0;
+            }
+        }
+    }
+}
